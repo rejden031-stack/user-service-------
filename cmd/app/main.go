e487ace006d54489d744384175c61267004fbdf1
Egorik <rejden031@gmail.com>
2026-05-28 21:28:40 +0300

    добавил горутины и канал в main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 	myhttp "user-service/internal/http"
 )
 
 func main() {
+	ch := make(chan string)
+
+	go func() {
+		for i := 0; i <= 5; i++ {
+			time.Sleep(1 * time.Second)
+
+			ch <- "Фоновое сообщение"
+		}
+	}()
+
+	go func() {
+		for msg := range ch {
+			log.Println("Получено:", msg)
+		}
+	}()
+
 	handler := myhttp.NewHandler()
 	http.HandleFunc("/user", handler.GetUser)
 
